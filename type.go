621c52a9d64f547468bb4129298881443c67313b
AriNoa <ikasumi.nekoneko@gmail.com>
2021-03-30 16:00:01 +0900

    💡 add comments

--- type.go
+++ type.go
@@ -4,15 +4,18 @@ import (
 	"errors"
 )
 
+// Handler is interface for command execution
 type Handler interface {
 	Handle(arg string)
 }
 
+// Router is structure that router for commands
 type Router struct {
 	childern map[string]*Router
 	handlers map[string]Handler
 }
 
+// New returns new router
 func New() *Router {
 	return &Router{
 		map[string]*Router{},
@@ -20,6 +23,7 @@ func New() *Router {
 	}
 }
 
+// AddRouter adds the child router
 func (r *Router) AddRouter(command string, router *Router) error {
 	if _, ok := r.childern[command]; ok {
 		return errors.New("Router for the command already exists")
@@ -30,6 +34,7 @@ func (r *Router) AddRouter(command string, router *Router) error {
 	return nil
 }
 
+// AddHandler adds the handler
 func (r *Router) AddHandler(command string, handler Handler) error {
 	if _, ok := r.handlers[command]; ok {
 		return errors.New("Handler for the command already exists")
@@ -40,6 +45,7 @@ func (r *Router) AddHandler(command string, handler Handler) error {
 	return nil
 }
 
+// Route handles the command
 func (r *Router) Route(str string) {
 	cmd, arg := DetachCommandFrom(str)
 
